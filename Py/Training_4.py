a83554d83825152c7a9606a0e5ae349114a8ea3f
luqman100296 <luqman100296@gmail.com>
2026-09-07 20:38:14 +0800

    Push 5

--- Py/Training_4.py
+++ Py/Training_4.py
@@ -1,3 +1,29 @@
 x = 3
 print(x == 3)
 print(x != 3)
+
+weight = int(input("Please enter your weight: "))
+height = float(input("Please enter your height (in meter): "))
+
+if weight >= 30:
+    print("Weight Accepted")
+else:
+    print("Please Enter Valid Weight")
+
+if height >= 1.0:
+    print("Height Accepted")
+else:
+    print("Please Enter Valid Height")
+
+BMI = weight/height**2
+
+print(BMI)
+
+if BMI <= 18.5:
+    print("Underweight")
+elif BMI <= 24.9:
+    print("Normal Weight")
+elif BMI <= 29.9:
+    print("Overweight")
+else:
+    print("Obese")
---
+++ Py/Training_5.py
@@ -0,0 +1,50 @@
+# For Loops, Lists and Sets
+
+# For Loop (Multiplication Table)
+# num = int(input("Enter number: "))
+
+# for i in range(1, 11):
+#     print(f"{num} x {i} = {num*i}")
+
+# While Loop (Multiplication Table)
+# num = int(input("Enter number: "))
+
+# i = 1
+# while i <= 10:
+#     print(f"{num} x {i} = {num*i}")
+#     i += 1
+
+# While Loop (Prime Numbers)
+
+# for num in range(2, 21):
+#     is_prime = True
+#     for i in range(2, int(num**0.5) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+#     if is_prime:
+#         print(num)
+
+# grocery = ["Fish", "Meat", "Broccoli", "Watermelon"]
+
+# print(grocery)
+# grocery.append("Rice")
+# grocery.remove("Fish")
+# grocery[2] = "Cabbage"
+# print(grocery)
+
+# numbers = [12, 45, 3, 67, 23, 1, 89]
+
+# biggest = numbers[0]
+# smallest = numbers[0]
+
+# for n in numbers:
+#     if n > biggest:
+#         biggest = n
+#     if n < smallest:
+#         smallest = n
+        
+
+# print(biggest)
+# print(smallest)
+
